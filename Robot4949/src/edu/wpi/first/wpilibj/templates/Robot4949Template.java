b8bf6d173ad2b391c6bc6aca56f83b23f76abdbf
slavalle <slavalle@IVYL46RNHK1.acct04.us.lmco.com>
2013-11-27 23:26:46 -0500

    Test of new Java station build

---
+++ Robot4949/src/edu/wpi/first/wpilibj/templates/Robot4949Template.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SimpleRobot; 
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot4949Template extends SimpleRobot {
+    RobotDrive drive = new RobotDrive(1, 2); 
+    Joystick leftStick = new Joystick(1); 
+    Joystick rightStick = new Joystick(2);
+    
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomous() {
+        for (int i = 0; i < 4; i++) { 
+            drive.drive(0.5, 0.0);      // drive 50% fwd 0% turn 
+            Timer.delay(2.0); // wait 2 seconds 
+            drive.drive(0.0, 0.75); // drive 0% fwd, 75% turn 
+        } 
+        drive.drive(0.0, 0.0); // drive 0% forward, 0% turn
+    }
+        
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        while (true && isOperatorControl() && isEnabled()) // loop until change 
+        { 
+            drive.tankDrive(leftStick, rightStick); // drive with joysticks 
+            Timer.delay(0.005); 
+        }
+
+    }
+    
+    /**
+     * This function is called once each time the robot enters test mode.
+     */
+    public void test() {
+    
+    }
+}
